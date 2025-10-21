aaeff6691357a61bbce6158756af639ce0e63cdc
Alex Huatay Casas <85271363+IDozy@users.noreply.github.com>
2025-10-21 21:14:25 +0000

    modificamos allowed origins para usar codespaces

---
+++ backend/config/cors.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'https://solid-space-happiness-v7vwxp5r44wf69qj-5173.app.github.dev',
+        'https://solid-space-happiness-v7vwxp5r44wf69qj-8000.app.github.dev',
+    ],
+   // 'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
