c4845e39369f11c4c182ff81c00c392d1cafc0f4
Alex Huatay Casas <85271363+IDozy@users.noreply.github.com>
2025-11-04 21:17:39 +0000

    configuracion cors

---
+++ backend/config/cors.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://solid-space-happiness-v7vwxp5r44wf69qj-5173.app.github.dev',
+        'http://localhost:5173',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
