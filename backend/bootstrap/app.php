3d6fb0f66d614576ff7348c7002423c70d30b723
Alex Huatay Casas <85271363+IDozy@users.noreply.github.com>
2025-11-04 21:17:48 +0000

    agregamos cors

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -6,12 +6,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
+
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
+
         $middleware->group('api', [EnsureFrontendRequestsAreStateful::class]);
         $middleware->validateCsrfTokens(except: [
             'api/*',
